813f79c0d9696345beb677cf11f291661bad42c3
Thari-SL <106327315+Thari-SL@users.noreply.github.com>
2022-08-17 09:28:55 +0530

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,53 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./Components/navigation/home/home.component";
+import {TeamComponent} from "./Components/navigation/team/team.component";
+import {GalleryComponent} from "./Components/navigation/gallery/gallery.component";
+import {FacilitiesComponent} from "./Components/navigation/facilities/facilities.component";
+import {ContactUsComponent} from "./Components/navigation/contact-us/contact-us.component";
+import {ActivitiesComponent} from "./Components/navigation/activities/activities.component";
+import {NavigationComponent} from "./Components/navigation/navigation.component";
+import {AuthContextComponent} from "./Components/auth-context/auth-context.component";
+import {LoginComponent} from "./Components/auth-context/login/login.component";
+import {SignupComponent} from "./Components/auth-context/signup/signup.component";
+import {DashboardPatientComponent} from "./Components/dashboard-patient/dashboard-patient.component";
+import {DashboardStaffComponent} from "./Components/dashboard-staff/dashboard-staff.component";
+import {DashboardDoctorComponent} from "./Components/dashboard-doctor/dashboard-doctor.component";
+import {DashboardStudentComponent} from "./Components/dashboard-student/dashboard-student.component";
+import {PatientContextComponent} from "./Components/dashboard-staff/Components/patient-context/patient-context.component";
+import {DoctorContextComponent} from "./Components/dashboard-staff/Components/doctor-context/doctor-context.component";
+import {StaffContextComponent} from "./Components/dashboard-staff/Components/staff-context/staff-context.component";
+import {StudentContextComponent} from "./Components/dashboard-staff/Components/student-context/student-context.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',redirectTo:'/navigation/home',pathMatch:'full'},
+  {
+    path: 'navigation', component: NavigationComponent, children: [
+      {path: 'home', component: HomeComponent},
+      {path: 'team', component: TeamComponent},
+      {path: 'gallery', component: GalleryComponent},
+      {path: 'facilities', component: FacilitiesComponent},
+      {path: 'contactUs', component: ContactUsComponent},
+      {path: 'activities', component: ActivitiesComponent}
+    ]
+  },
+  {path:'auth-context',component:AuthContextComponent,children:[
+      {path: 'login',component: LoginComponent},
+      {path: 'signup',component: SignupComponent}
+    ]
+  },
+  {path:'dashboard-patient',component:DashboardPatientComponent},
+
+  {path:'dashboard-doctor',component:DashboardDoctorComponent},
+  {path:'dashboard-student',component:DashboardStudentComponent},
+
+  {path:'dashboard-staff',component:DashboardStaffComponent,children:[
+      {path: 'patient-context',component: PatientContextComponent},
+      {path: 'doctor-context',component:  DoctorContextComponent},
+      {path: 'staff-context',component: StaffContextComponent},
+      {path: 'student-context',component: StudentContextComponent}
+    ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,108 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from "@angular/router";
+
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatSelectModule} from "@angular/material/select";
+import {MatNativeDateModule, MatOptionModule} from "@angular/material/core";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {MatPaginatorModule} from "@angular/material/paginator";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './Components/navigation/navigation.component';
+import { HomeComponent } from './Components/navigation/home/home.component';
+import { FacilitiesComponent } from './Components/navigation/facilities/facilities.component';
+import { ContactUsComponent } from './Components/navigation/contact-us/contact-us.component';
+import { TeamComponent } from './Components/navigation/team/team.component';
+import { GalleryComponent } from './Components/navigation/gallery/gallery.component';
+import { ActivitiesComponent } from './Components/navigation/activities/activities.component';
+import { AuthContextComponent } from './Components/auth-context/auth-context.component';
+import { LoginComponent } from './Components/auth-context/login/login.component';
+import { SignupComponent } from './Components/auth-context/signup/signup.component';
+import { DashboardPatientComponent } from './Components/dashboard-patient/dashboard-patient.component';
+import { DashboardStaffComponent } from './Components/dashboard-staff/dashboard-staff.component';
+import { DashboardDoctorComponent } from './Components/dashboard-doctor/dashboard-doctor.component';
+import { DashboardStudentComponent } from './Components/dashboard-student/dashboard-student.component';
+import { TopBarComponent } from './Components/dashboard-patient/items/top-bar/top-bar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopBarDoctorComponent } from './Components/dashboard-doctor/items/top-bar-doctor/top-bar-doctor.component';
+import { TopBarStaffComponent } from './Components/dashboard-staff/items/top-bar-staff/top-bar-staff.component';
+import { TopBarStudentComponent } from './Components/dashboard-student/items/top-bar-student/top-bar-student.component';
+import { PatientContextComponent } from './Components/dashboard-staff/Components/patient-context/patient-context.component';
 
+import { SeePatientsComponent } from './Components/dashboard-staff/Components/patient-context/see-patients/see-patients.component';
+import { SavePatientsComponent } from './Components/dashboard-staff/Components/patient-context/save-patients/save-patients.component';
+import { UpdatePatientsComponent } from './Components/dashboard-staff/Components/patient-context/update-patients/update-patients.component';
+import { DeletePatientsComponent } from './Components/dashboard-staff/Components/patient-context/delete-patients/delete-patients.component';
+import { SearchPatientComponent } from './Components/dashboard-staff/Components/patient-context/search-patient/search-patient.component';
+import { StaffContextComponent } from './Components/dashboard-staff/Components/staff-context/staff-context.component';
+import { DoctorContextComponent } from './Components/dashboard-staff/Components/doctor-context/doctor-context.component';
+import { StudentContextComponent } from './Components/dashboard-staff/Components/student-context/student-context.component';
+
+// @ts-ignore
+// @ts-ignore
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    FacilitiesComponent,
+    ContactUsComponent,
+    TeamComponent,
+    GalleryComponent,
+    ActivitiesComponent,
+    AuthContextComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardPatientComponent,
+    DashboardStaffComponent,
+    DashboardDoctorComponent,
+    DashboardStudentComponent,
+    TopBarComponent,
+    TopBarDoctorComponent,
+    TopBarStaffComponent,
+    TopBarStudentComponent,
+    PatientContextComponent,
+
+    SeePatientsComponent,
+    SavePatientsComponent,
+    UpdatePatientsComponent,
+    DeletePatientsComponent,
+    SearchPatientComponent,
+    StaffContextComponent,
+    DoctorContextComponent,
+    StudentContextComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatOptionModule,
+    MatInputModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatOptionModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatMenuModule,
+    BrowserAnimationsModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
